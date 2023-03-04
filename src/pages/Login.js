ee3e49a53f6f080a5090442c13b6d80df57dee69
Matt Villa <matthewvilla805@gmail.com>
2023-03-03 23:03:24 -0800

    Fixed bug on login recording

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { auth, db, provider } from "../firebase_setup/firebase";
 import { signInWithPopup } from "firebase/auth";
-import { addDoc, collection} from "firebase/firestore";
+import { addDoc, collection, doc, setDoc } from "firebase/firestore";
 import MenuHeader from "../components/MenuHeader";
 import "./Login.scss";
 import navPages from "../nav-directory";
@@ -13,17 +13,24 @@ const Login = () => {
   const logUserIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-        addDoc(
-          collection(db, "users"),
+        // console.log(result.user);
+        const userRef = doc(db, 'users', `${result.user.email}`);
+        console.log(userRef);
+        setDoc(
+          userRef,
           {
             id: result.user.uid,
             email: result.user.email,
             lastLogin: Date(),
-          }
-        );
-        navigate(`${navPages.Home(result.user.uid)}`);
+          },
+          { merge: true }
+        ).catch((error) => {
+          alert(`DB error: ${error}`);
+          navigate(`${navPages.Main}`);
+        });
+        return result;
       })
-      .then((result)=>{
+      .then((result) => {
         navigate(`${navPages.Home(result.user.uid)}`);
       })
       .catch((error) => {
